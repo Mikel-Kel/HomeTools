38a38b30fb39008843113cb94b8c2ff6fe730700
Michel Vuilleumier <michelwv@hispeed.ch>
2026-03-14 21:44:17 +0100

    Release v2.10.9 – Toujours...

--- src/composables/useParties.ts
+++ src/composables/useParties.ts
@@ -19,7 +19,32 @@ export function useParties() {
 
   const { folders } = useDrive();
 
-  async function load(): Promise<void> {
+
+async function load(): Promise<void> {
+
+  const raw = await loadJSONFromFolder<any>(
+    folders.value.settings,
+    "Parties.json"
+  );
+
+  console.log("PARTIES RAW =", raw);
+  console.log("TYPEOF RAW =", typeof raw);
+
+  if (raw) {
+    console.log("RAW KEYS =", Object.keys(raw));
+    console.log("RAW.VERSION =", raw.version);
+    console.log("RAW.PARTIES =", raw.parties);
+    console.log("IS ARRAY =", Array.isArray(raw.parties));
+  }
+
+  if (!raw || !Array.isArray(raw.parties)) {
+    throw new Error("Invalid Parties.json format");
+  }
+
+  parties.value = raw.parties;
+}
+
+/*  async function load(): Promise<void> {
 
     const raw = await loadJSONFromFolder<PartiesFile>(
       folders.value.settings,
@@ -32,7 +57,7 @@ export function useParties() {
 
     parties.value = raw.parties;
 
-  }
+  }*/
 
   function getParty(id: number): Party | undefined {
     return parties.value.find(p => p.id === id);
